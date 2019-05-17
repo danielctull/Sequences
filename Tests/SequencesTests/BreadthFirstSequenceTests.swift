c12d55fa3f3acd4bf95807be22317099106c900a
Daniel Tull <dt@danieltull.co.uk>
2019-05-17 23:23:26 +0100

    Embed test types into the test functions

--- Tests/SequencesTests/BreadthFirstSequenceTests.swift
+++ Tests/SequencesTests/BreadthFirstSequenceTests.swift
@@ -2,20 +2,18 @@
 import XCTest
 import Sequences
 
-fileprivate struct Item {
-    let name: String
-    let children: [Item]
-}
-
-extension Item {
-    var hierarchy: BreadthFirstSequence<Item> {
-        return BreadthFirstSequence(initial: self, children: { $0.children })
-    }
-}
-
 final class BreadthFirstSequenceTests: XCTestCase {
 
     func test() {
+
+        struct Item {
+            let name: String
+            let children: [Item]
+
+            var hierarchy: BreadthFirstSequence<Item> {
+                return BreadthFirstSequence(initial: self, children: { $0.children })
+            }
+        }
 
         let item = Item(name: "0", children: [
             Item(name: "0.0", children: [
--- Tests/SequencesTests/LinkSequenceTests.swift
+++ Tests/SequencesTests/LinkSequenceTests.swift
@@ -4,17 +4,18 @@ import XCTest
 
 final class LinkSequenceTests: XCTestCase {
 
-    final class View {
-        let name: String
-        let superview: View?
-
-        init(name: String, superview: View? = nil) {
-            self.name = name
-            self.superview = superview
-        }
-    }
-
     func test() {
+
+        final class View {
+            let name: String
+            let superview: View?
+
+            init(name: String, superview: View? = nil) {
+                self.name = name
+                self.superview = superview
+            }
+        }
+
         let view = View(name: "1", superview: View(name: "2", superview: View(name: "3")))
         let sequence = LinkSequence(initial: view, next: { $0.superview })
         XCTAssertEqual(Array(sequence.map { $0.name }), ["1", "2", "3"])
